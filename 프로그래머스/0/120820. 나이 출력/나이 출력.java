1098162b71f252723fccb22268bcbf2a8ad4cf2e
blue <162953206+bluewt129@users.noreply.github.com>
2024-03-25 18:57:20 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        for(int i=1;i<=120;i++){
+            answer = 2022-age+1;
+        }
+        return answer;
+    }
+}
