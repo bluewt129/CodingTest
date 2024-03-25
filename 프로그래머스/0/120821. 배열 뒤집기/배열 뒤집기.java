4def2461b94a0ebce4bc2d0d19c01f583a56b1f8
blue <162953206+bluewt129@users.noreply.github.com>
2024-03-25 20:46:37 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        for(int i=0;i<num_list.length/2;i++){
+            int tmp = 0;
+            tmp = num_list[i];
+            num_list[i] = num_list[num_list.length-1-i];
+            num_list[num_list.length-1-i] = tmp;
+        }
+        return num_list;
+    }
+}
